ba43621310393b7e3130287c32fb133917d87a20
Thomas Margraff <thomaspmar@protonmail.com>
2022-07-10 12:10:51 -0700

    Initial Commit

---
+++ NoMansSky.ModTemplate/ColorData.cs
@@ -0,0 +1,81 @@
+﻿using libMBIN.NMS;
+using Reloaded.ModHelper;
+
+namespace NoMansSky.ModTemplate
+{
+    class ColorData
+    {
+        const float colorChangeAmount = 0.003f;
+        private bool rCountingDown;
+        private bool gCountingDown;
+        private bool bCountingDown;
+
+        private float previousR;
+        private float previousG;
+        private float previousB;
+
+        private bool isInit = false;
+
+        public ColorData()
+        {
+
+        }
+
+        public ColorData(float r, float g, float b)
+        {
+            previousR = r;
+            previousG = g;
+            previousB = b;
+            isInit = true;
+        }
+
+        public Colour ModifyColor(Colour colorToModify)
+        {
+            if (!isInit)
+            {
+                previousR = colorToModify.R;
+                previousG = colorToModify.G;
+                previousB = colorToModify.B;
+                isInit = true;
+            }
+
+            colorToModify.R += rCountingDown ? -colorChangeAmount : colorChangeAmount;
+            if (colorToModify.R <= 0f)
+            {
+                colorToModify.R = 0f;
+                rCountingDown = false;
+            }
+            else if (colorToModify.R >= 1f)
+            {
+                colorToModify.R = 1f;
+                rCountingDown = true;
+            }
+
+            colorToModify.G += gCountingDown ? -colorChangeAmount : colorChangeAmount;
+            if (colorToModify.G <= 0f)
+            {
+                colorToModify.G = 0f;
+                gCountingDown = false;
+            }
+            else if (colorToModify.G >= 1f)
+            {
+                colorToModify.G = 1f;
+                gCountingDown = true;
+            }
+
+            colorToModify.B += bCountingDown ? -colorChangeAmount : colorChangeAmount;
+            if (colorToModify.B <= 0f)
+            {
+                colorToModify.B = 0f;
+                bCountingDown = false;
+            }
+            else if (colorToModify.B >= 1f)
+            {
+                colorToModify.B = 1f;
+                bCountingDown = true;
+            }
+
+            return colorToModify;
+        }
+    }
+}
--- NoMansSky.ModTemplate/Mod.cs
+++ NoMansSky.ModTemplate/Mod.cs
@@ -5,6 +5,7 @@ using NoMansSky.Api;
 using libMBIN.NMS;
 using libMBIN.NMS.Globals;
 using libMBIN.NMS.GameComponents;
+using System.Collections.Generic;
 
 namespace NoMansSky.ModTemplate
 {
@@ -13,60 +14,103 @@ namespace NoMansSky.ModTemplate
     /// </summary>
     public class Mod : NMSMod
     {
+        int framesBetweenUpdates = 3;
+        int frameCount = 0;
+
+        Dictionary<string, ColorData> colorDataByName = new Dictionary<string, ColorData>()
+        {
+            { "CloudColour1", new ColorData() },
+            { "CloudColour2", new ColorData() },
+            { "FogColour", new ColorData() },
+            { "HeightFogColour", new ColorData() },
+            { "HorizonColour", new ColorData() },
+            { "LightColour", new ColorData() },
+            { "SkyColour", new ColorData() },
+            { "SkySolarColour", new ColorData() },
+            { "SkyUpperColour", new ColorData() },
+            { "SunColour", new ColorData() }
+        };
+
         /// <summary>
         /// Initializes your mod along with some necessary info.
         /// </summary>
         public Mod(IModConfig _config, IReloadedHooks _hooks, IModLogger _logger) : base(_config, _hooks, _logger)
         {
-            // This is how to log a message to the Reloaded II Console.
-            Logger.WriteLine("Hello World!");
-
-
-            // The API relies heavily on Mod Events.
-            // Below are 4 examples of using them.
-            Game.OnProfileSelected += () => Logger.WriteLine("The player just selected a save file");
-            Game.OnMainMenu += OnMainMenu;            
-            Game.OnGameJoined.AddListener(GameJoined);
-            Game.OnWarpFinished.AddListener(() =>
-            {
-                var system = CurrentSystem.GetSystemData();
-                Logger.WriteLine($"Loaded into system: {system.Name}");
-            });
+            
         }
 
         /// <summary>
         /// Called once every frame.
         /// </summary>
-        public override void Update()
+        public async override void Update()
         {
-            // Here is an example of checking for keyboard keys
-            if (Keyboard.IsPressed(Key.UpArrow))
+            if (!Game.IsInGame)
+                return;
+
+            frameCount++;
+            if (frameCount <= framesBetweenUpdates)
+                return;
+
+            await Game.Colors.DaySkyColors.ModifyAsync<GcWeatherColourSettings>(colorSettings =>
             {
-                Logger.WriteLine("The Up Arrow was just pressed!");
+                foreach (var setting in colorSettings.GenericSettings.Settings)
+                {
+                    ModifyColorSetting(setting);
+                }
+            });
+
+            await Game.Colors.DuskSkyColors.ModifyAsync<GcWeatherColourSettings>(colorSettings =>
+            {
+                foreach (var setting in colorSettings.GenericSettings.Settings)
+                {
+                    ModifyColorSetting(setting);
+                }
+            });
+
+            await Game.Colors.NightSkyColors.ModifyAsync<GcWeatherColourSettings>(colorSettings =>
+            {
+                foreach (var setting in colorSettings.GenericSettings.Settings)
+                {
+                    ModifyColorSetting(setting);
+                }
+            });
+
+            frameCount = 0;
+        }
+
+        /// <summary>
+        /// Handles the logic for modifying each color.
+        /// </summary>
+        /// <param name="colorSetting"></param>
+        private void ModifyColorSetting(GcPlanetWeatherColourData colorSetting)
+        {
+            foreach (var color in colorSetting.GetAllColours())
+            {
+                var data = GetColorData(color.Key, color.Value);
+                if (data == null)
+                    continue;
+
+                var result = data.ModifyColor(color.Value);
+                color.Value.R = result.R;
+                color.Value.G = result.G;
+                color.Value.B = result.B;
             }
         }
 
-        private void OnMainMenu()
+        /// <summary>
+        /// Returns color data for the color with the provided setting name.
+        /// </summary>
+        /// <param name="settingName"></param>
+        /// <param name="colour"></param>
+        /// <returns></returns>
+        private ColorData GetColorData(string settingName, Colour colour)
         {
-            Logger.WriteLine("Main Menu shown!");
+            if (colorDataByName.TryGetValue(settingName, out var colorData))
+                return colorData;
 
-
-            MbinModdingExample();
-        }
-
-        // here is an example of modding globals with the new MemoryManager
-        private void MbinModdingExample()
-        {
-            // example of getting the run speed from the player globals
-            float currentRunSpeed = GetValue<float>("GcPlayerGlobals.GroundRunSpeed");
-
-            // example of settng the run speed to twice it's original value.
-            SetValue("GcPlayerGlobals.GroundRunSpeed", currentRunSpeed * 2);
-        }
-
-        private void GameJoined()
-        {
-            Logger.WriteLine("The game was joined!");
+            colorData = new ColorData(colour.R, colour.G, colour.B);
+            colorDataByName.Add(settingName, colorData);
+            return colorData;
         }
     }
 }
